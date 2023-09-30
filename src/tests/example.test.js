99bb3b981b641adfab5b6cc9578e5afe22ffbd31
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-09-30 14:32:51 +0900

    add Mocha-steps

--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -1,5 +1,33 @@
-describe('Config test', () => {
-  it('should work', async () => {
-    console.log("Yeay")
+import { step } from 'mocha-steps'
+import puppeteer  from 'puppeteer'
+
+describe('Mocha steps demo', () => {
+  let browser
+  let page
+
+  before(async () => {
+    browser = await puppeteer.launch({ headless: true })
+    page = await browser.newPage()
+    await page.setDefaultTimeout(7000)
+  })
+
+  after(async () => {
+    await browser.close()
+  })
+
+  step('should load google homepage', async () => {
+    await page.goto('http://www.google.com')
+  })
+  
+  step("step 2 should fail", async () => {
+    await page.waitForSelector("#FAIL")
+  })
+
+  step("step 3", async () =>{
+    console.log("From step 3")
+  })
+
+  step("step 3", async () =>{
+    console.log("From step 4")
   })
 })
