08c7a1072a7f078b21a3ceda172cef4ae810fc37
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-09-30 15:09:41 +0900

    Framework Core - Builder class - set viewport

--- src/builder.js
+++ src/builder.js
@@ -16,6 +16,22 @@ export default class Builder {
     const page = await browser.newPage()
     const extendedPage = new Builder(page)
     await page.setDefaultTimeout(10000)
-    
+
+    switch(viewport) {
+      case "Mobile":
+        const mobileViewport = puppeteer.KnownDevices['iPhone X']
+        await page.emulate(mobileViewport)
+        break
+      case "Tablet":
+        const tabletViewport = puppeteer.KnownDevices['iPad landscape']
+        await page.emulate(tabletViewport)
+        break
+      case "Desktop":
+        await page.setViewport({ width: 1920, height: 1080})
+        break
+      default:
+        throw new Error('Supported devices are only Mobile | Tablet | Desktop')
+    }
+
   }
 }
